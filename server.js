a83bbd85f3c5e2828464c598a5afcf09fb8cc5b1
Davi Prada Hoffmann <davi.phoffmann@gmail.com>
2020-05-13 08:43:06 -0300

    adicionado mongoose para fazer a conexão com o banco de dados

--- server.js
+++ server.js
@@ -1,9 +1,18 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
+// Inicia o APP
 const app = express();
 
+// Inicia o DB
+mongoose.connect('mongodb://localhost:27017/nodeapi', { 
+    useUnifiedTopology: true, 
+    useNewUrlParser: true 
+});
+
+// Primeira Rota
 app.get('/', (req, res) => {
-    res.send('Hello');
+    res.send('Hello Teste');
 });
 
 app.listen(3001);
